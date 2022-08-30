caf907d33df5f7c802ac050f36cbf78c80ae81e7
Pedro <pedrosteinmuller10105@hotmail.com>
2022-08-30 18:14:38 -0300

    refazendo o requisito 3, pois esqueci de adicionar o header em cada rota que o requisito 3 pede e adicionado exact nas rotas no app.js, em suma, refatoracao dos codigos e deixando mais organizado

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
-import Header from './components/Header';
 
 class App extends React.Component {
   render() {
@@ -16,14 +15,13 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="*" component={ NotFound } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="*" component={ NotFound } />
         </Switch>
-        <Header />
-        <Route path="/profile/edit" component={ ProfileEdit } />
       </BrowserRouter>
     );
   }
