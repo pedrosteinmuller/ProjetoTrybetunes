6a8cfd172fbb39028fac017e25ace7dfa829c322
Pedro <pedrosteinmuller10105@hotmail.com>
2022-08-30 20:25:24 -0300

    resolucao requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    searchArtistName: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      searchArtistName: value,
+    });
+  };
+
   render() {
+    const { searchArtistName } = this.state;
+    const minlength = 2;
     return (
       <div data-testid="page-search">
+        <form>
+          <label htmlFor="search-artist-input">
+            Nome:
+            <input
+              id="search-artist-input"
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ searchArtistName.length < minlength }
+          >
+            Pesquisar
+          </button>
+        </form>
         <Header />
       </div>
     );
