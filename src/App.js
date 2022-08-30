060b27fa0321cdff85883d4774b77a877fa70bdb
Pedro <pedrosteinmuller10105@hotmail.com>
2022-08-30 17:10:49 -0300

    resolvendo problema do requisito 1 após implementação do Switch, não entendi o por que, mas o que fiz deu certo

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends React.Component {
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="*" component={ NotFound } />
         </Switch>
+        <Route path="/profile/edit" component={ ProfileEdit } />
 
       </BrowserRouter>
     );
