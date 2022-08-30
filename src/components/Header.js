b9ea63a8a7403bf31a94573cfb6fab06555da60e
Pedro <pedrosteinmuller10105@hotmail.com>
2022-08-30 18:53:48 -0300

    resolucao requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -34,6 +35,24 @@ class Header extends Component {
     const { userName, loading } = this.state;
     return (
       <header data-testid="header-component">
+        <Link
+          to="/search"
+          data-testid="link-to-search"
+        >
+          Pesquisa
+        </Link>
+        <Link
+          to="/favorites"
+          data-testid="link-to-favorites"
+        >
+          Favoritas
+        </Link>
+        <Link
+          to="/profile"
+          data-testid="link-to-profile"
+        >
+          Perfil
+        </Link>
         {loading
           ? (
             <Loading />
